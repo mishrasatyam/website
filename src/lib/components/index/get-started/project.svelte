a3d2d14c4bb1758509b41a47a03dd8731d69b691
Julian <julian@gitpod.io>
2022-02-04 12:11:43 +0000

    feat: update project-buttons

--- src/lib/components/index/get-started/project.svelte
+++ src/lib/components/index/get-started/project.svelte
@@ -1,90 +1,17 @@
 <script lang="ts">
   import type { Project } from "$lib/types/project.type";
+  import LinkButton from "$lib/components/ui-library/link-button";
+  import Button from "$lib/components/ui-library/button";
 
   export let project: Project;
 
   const { logo, alt, title, githubUrl, trackingName } = project;
 </script>
 
-<style lang="scss">
-  .project {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0.75rem 0.75rem 0.75rem var(--micro);
-    background: var(--off-white);
-    border-radius: 0.75rem;
-    transition: all 0.2s;
-
-    @media (max-width: 454px) {
-      padding: var(--macro) var(--micro);
-    }
-
-    &__left {
-      display: flex;
-      place-items: center;
-    }
-
-    &__logo {
-      height: 1.6875rem;
-      width: 1.6875rem;
-      margin-right: var(--micro);
-      filter: grayscale(100%) opacity(0.5);
-      -webkit-filter: grayscale(100%) opacity(0.5);
-      transition: all 0.2s;
-
-      @media (max-width: 454px) {
-        width: 25px;
-        margin-right: var(--macro);
-      }
-    }
-
-    &:not(:last-child) {
-      margin-bottom: 0.75rem;
-    }
-
-    &:hover,
-    &:focus {
-      background: var(--brand-light);
-
-      .project__logo {
-        filter: grayscale(0) opacity(1);
-        -webkit-filter: grayscale(0) opacity(1);
-      }
-
-      .btn-tertiary {
-        opacity: 1;
-      }
-    }
-  }
-
-  .btn-tertiary {
-    display: inline-block;
-    font-size: var(--btn-small);
-    font-weight: 600;
-    line-height: 16px;
-    text-align: center;
-    display: inline-block;
-    background-color: var(--black);
-    color: var(--white);
-
-    &:hover,
-    &:focus {
-      color: var(--white);
-      background-color: var(--black-hover);
-    }
-    border-radius: 0.75rem;
-    max-width: 9rem;
-    padding: 0.5rem 0.75rem;
-    opacity: 0;
-
-    @media (max-width: 500px) {
-      display: none;
-    }
-  }
-</style>
-
-<a
+<LinkButton
+  class="flex h-14 items-center justify-between group"
+  variant="cta"
+  size="large"
   href={`https://gitpod.io#${githubUrl}`}
   target="_blank"
   aria-label={alt}
@@ -92,15 +19,24 @@
     trackingName +
     `"}`}
   rel="noopener"
-  class="project group"
 >
-  <div class="project__left">
-    <img src={`/${logo}`} {alt} class="project__logo" width="27" height="27" />
-    <p
-      class="leading-5 text-h6 text-gray-800 font-semibold transition duration-200 group-focus:text-black group-hover:text-black"
+  <div class="flex items-center">
+    <img
+      src={`/${logo}`}
+      {alt}
+      class="mr-micro transition duration-200 grayscale group-focus:grayscale-0 group-hover:grayscale-0"
+      width="24"
+      height="24"
+    />
+    <div
+      class="text-gray-800 transition duration-200 group-focus:text-black group-hover:text-black"
     >
       {title}
-    </p>
+    </div>
   </div>
-  <span class="btn-tertiary">Launch workspace</span>
-</a>
+  <Button
+    class="hidden group-hover:inline-block"
+    variant="tertiary"
+    size="medium">Launch workspace</Button
+  >
+</LinkButton>
